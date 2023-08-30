d4c4efd40787c53014c973fb505612651519a321
kennaheng19 <rickyheng19@gmail.com>
2023-08-30 02:36:27 -0500

    Created packageLoader file to read from csv file

--- hashmap.py
+++ hashmap.py
@@ -1,63 +1,73 @@
-#Code referenced from this video: https://www.youtube.com/watch?v=9HFbhPscPU0
+# code referenced from https://www.youtube.com/watch?v=9HFbhPscPU0&ab_channel=JoeJames
 
+# Hash Map
 class HashMap:
-        #Constructor. Creates an array to store data.
-        def __init__(self):
-                self.size = 64
-                self.map = [None] * self.size
-		
-        #Takes a key, turns it into an index and returns it
-        def _get_hash(self, key):
-                hash = 0
-                for char in str(key):
-                        hash += ord(char)
-                return hash % self.size
+    def __init__(self):
+        # Initialize the size of the hash map and create an empty array to store data
+        self.size = 50
+        self.map = [None] * self.size
         
-		#Gets index from get_hash function. Make list from key and value
-        def add(self, key, value):
-                key_hash = self._get_hash(key)
-                key_value = [key, value]
-                #if key_hash list is empty, insert the key and value provided
-                if self.map[key_hash] is None:
-                        self.map[key_hash] = list([key_value])
-                        return True
-                #else if key exist, then just update with value provided
-                else:
-                        for pair in self.map[key_hash]:
-                                if pair[0] == key:
-                                        pair[1] = value
-                                        return True
-                        self.map[key_hash].append(key_value)
-                        return True
-		#get value of key provided	
-        def get(self, key):
-                key_hash = self._get_hash(key)
-                if self.map[key_hash] is not None:
-                        for pair in self.map[key_hash]:
-                                if pair[0] == key:
-                                        return pair[1]
-                return None
-		#Delete key
-        def delete(self, key):
-                key_hash = self._get_hash(key)
-		
-                if self.map[key_hash] is None:
-                        return False
-                for i in range (0, len(self.map[key_hash])):
-                        if self.map[key_hash][i][0] == key:
-                                self.map[key_hash].pop(i)
-                                return True
-                return False
-	
-        def keys(self):
-                arr = []
-                for i in range(0, len(self.map)):
-                        if self.map[i]:
-                                arr.append(self.map[i][0])
-                return arr
+    def _get_hash(self, key):
+        # Calculate the hash value for a given key by summing the ASCII values of its characters
+        hash = 0
+        for char in str(key):
+            hash += ord(char)
+        return hash % self.size  # Ensure the hash fits within the size of the map
+        
+    def add(self, key, value):
+        # Add a key-value pair to the hash map
+        key_hash = self._get_hash(key)
+        key_value = [key, value]
+        
+        if self.map[key_hash] is None:
+            # If the slot at the hash index is empty, create a new list for the key-value pair
+            self.map[key_hash] = list([key_value])  # type: ignore
+            return True
+        else:
+            # If the slot is not empty, search for the key and update its value if found,
+            # or add a new key-value pair if the key is not already present
+            for pair in self.map[key_hash]:  # type: ignore
+                if pair[0] == key:
+                    pair[1] = value
+                    return True
+            self.map[key_hash].append(key_value)  # type: ignore
+            return True
+    
+    def get(self, key):
+        # Retrieve the value associated with a given key from the hash map
+        key_hash = self._get_hash(key)
+        if self.map[key_hash] is not None:
+            for pair in self.map[key_hash]:  # type: ignore
+                if pair[0] == key:
+                    return pair[1]
+        return None
+    
+    def delete(self, key):
+        # Delete a key-value pair from the hash map
+        key_hash = self._get_hash(key)
+        
+        if self.map[key_hash] is None:
+            return False
+        for i in range(0, len(self.map[key_hash])):  # type: ignore
+            if self.map[key_hash][i][0] == key:  # type: ignore
+                self.map[key_hash].pop(i)  # type: ignore
+                return True
+        return False
+    
+    def keys(self):
+        # Retrieve a list of all keys present in the hash map
+        arr = []
+        for i in range(0, len(self.map)):
+            if self.map[i]:
+                arr.append(self.map[i][0])  # type: ignore
+        return arr
+    
+    def printContents(self):
+        # Print the contents of the hash map
+        print("All Packages")
+        for item in self.map:
+            if item is not None:
+                print(str(item))
 			
-        def print(self):
-                print('---PHONEBOOK----')
-                for item in self.map:
-                        if item is not None:
-                                print(str(item))
+
+			
--- packageLoader.py
+++ packageLoader.py
@@ -2,28 +2,13 @@ import csv
 from package import Package
 from hashmap import HashMap
 
-def load_packages_from_csv(file_path):
-    package_list = []
+# Replace this with the actual path to your CSV file
+file_path = 'WGUPS Package File.csv'
 
-    with open(file_path, newline='', encoding='utf-8') as csvfile:
-        csvreader = csv.reader(csvfile)
-        next(csvreader)  # Skip the header line
-        for row in csvreader:
-            if len(row) == 9:
-                package_id, address, city, state, zip_code, deadline, weight, _, special_notes = row
-                package = Package(package_id, address, city, state, zip_code, deadline, weight, special_notes)
-                package_list.append(package)
-
-    return package_list
-
-def print_all_packages(package_list):
-    for package in package_list:
-        print(f"Package ID: {package.package_id}")
-        print(f"Address: {package.address}")
-        print(f"City: {package.city}")
-        print(f"State: {package.state}")
-        print(f"Zip Code: {package.zip_code}")
-        print(f"Deadline: {package.deadline}")
-        print(f"Weight: {package.weight}")
-        print(f"Special Notes: {package.special_notes}")
-        print("-" * 30)  # Just for formatting
+# Open the CSV file and create a CSV reader
+with open(file_path, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+     
+    # Iterate through each row in the CSV file
+    for row in csvreader:
+        print(row)
